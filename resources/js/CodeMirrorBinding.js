3e747451b08d2800feb1f24433f4702270cf3680
yann.lesage <yann.lesage@elamp.fr>
2021-10-11 15:11:34 +0200

    feat: working v1 with save callback

--- resources/js/CodeMirrorBinding.js
+++ resources/js/CodeMirrorBinding.js
@@ -1,6 +1,16 @@
-let cMirrorBinding
-
 class CMirrorBinding {
+  static instance
+
+  static initEditors() {
+    CMirrorBinding.getInstance().initEditors()
+  }
+
+  static getInstance() {
+    if(!CMirrorBinding.instance) {
+      CMirrorBinding.instance = new CMirrorBinding()
+    }
+    return CMirrorBinding.instance
+  }
 
   constructor() {
     this.editorsMaps = new WeakMap()
@@ -11,21 +21,28 @@ class CMirrorBinding {
     for (let container of containers) {
       let editorElement = container.getElementsByClassName("cmirror-container-editor")[0]
       editorElement.innerHTML = ""
-      let editor = CodeMirror(document.body, {
-        value: editorElement.dataset.code,
+      let editor = CodeMirror(editorElement, {
+        value: editorElement.dataset.code || " ",
         mode:  editorElement.dataset.language || "markdown"
       })
       this.editorsMaps.set(container, editor)
     }
   }
 
-  static getContentFor(saveButtonElement) {
+  getContentFor(saveButtonElement) {
+    let containerElement = saveButtonElement.parentElement
+    let editor = this.editorsMaps.get(containerElement)
+    if(editor) {
+      return editor.getValue()
+    } else {
+      console.error(new Error("editor not found"), {containerElement})
+    }
   }
 
   static save(saveButtonElement) {
     document.body.style.cursor = "progress"
     saveButtonElement.style.cursor = "not-allowed"
-    return this.getContentFor(saveButtonElement)
+    return CMirrorBinding.getInstance().getContentFor(saveButtonElement)
   }
 
   static saveCompleted(saveButtonElement) {
@@ -37,5 +54,8 @@ class CMirrorBinding {
     document.body.style.cursor = "default"
     saveButtonElement.style.cursor = "default"
   }
-
 }
+
+window.addEventListener("load", () => {
+  CMirrorBinding.initEditors()
+})
